bc209097bae57e40805070328df12e90ad968f42
vidhi udasi <udasividhi18@gmail.com>
2025-04-07 11:33:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.java
+++ Difficulty: Basic/Reverse a String/reverse-a-string.java
@@ -1,45 +1,30 @@
 //{ Driver Code Starts
-// Initial template for Java
+// Initial Template for Java
 
 import java.io.*;
+import java.lang.*;
 import java.util.*;
 
-public class GFG {
+class Driver {
     public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String s = read.readLine();
-            Solution ob = new Solution();
 
-            System.out.println(ob.revStr(s));
-        
-System.out.println("~");
-}
+        while (t-- > 0) {
+            String str = read.readLine();
+            System.out.println(new Solution().reverseString(str));
+
+            System.out.println("~");
+        }
     }
 }
 // } Driver Code Ends
 
+
+// User function Template for Java
+
 class Solution {
-    static String revStr(String s) {
-        // Convert the string to a character array
-        char[] charArray = s.toCharArray();
-        int start = 0;
-        int end = s.length() - 1;
-
-        // Swap characters from start and end until they meet in the middle
-        while (start < end) {
-            // Swap charArray[start] and charArray[end]
-            char temp = charArray[start];
-            charArray[start] = charArray[end];
-            charArray[end] = temp;
-
-            // Move the pointers towards the center
-            start++;
-            end--;
-        }
-
-        // Convert the character array back to a string
-        return new String(charArray);
+    public static String reverseString(String s) {
+     return new StringBuilder(s).reverse().toString();
     }
 }
