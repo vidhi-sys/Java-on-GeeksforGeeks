2552cf2e7054c1c3d72f54b9679f455396a00874
vidhi udasi <udasividhi18@gmail.com>
2025-04-14 10:28:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Modular Node/modular-node.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+
+/*node class of the linked list is as:
+class Node {
+    int data;
+     Node next;
+     Node(int key)
+     {
+         data = key;
+         next = null;
+     }
+}
+*/
+// Your task is to complete the function
+// function should return the modular Node
+// if no such node is present then return -1
+class Solution {
+    public int modularNode(Node head, int k) {
+        int index=-1;
+        int pos=0;
+        Node temp=head;
+        while(temp!=null){
+            pos++;
+            if( pos%k==0){
+              index=temp.data;}
+           
+            temp=temp.next;
+        }
+        return index;
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+public class GFG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            int k = Integer.parseInt(br.readLine());
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            int res = ob.modularNode(head, k);
+            System.out.println(res);
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
