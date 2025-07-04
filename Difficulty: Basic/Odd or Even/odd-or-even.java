06be6a88f2fde89fe453bc86d4f445ffcb3bde20
vidhi udasi <udasividhi18@gmail.com>
2025-07-04 11:16:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Odd or Even/odd-or-even.java
@@ -0,0 +1,11 @@
+class Solution {
+    static boolean isEven(int n) {
+        if((n &1)==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
