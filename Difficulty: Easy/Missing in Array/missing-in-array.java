d75bd84947965c6c14009b9b0b4054faf23b30ad
vidhi udasi <udasividhi18@gmail.com>
2025-07-22 22:41:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,39 +1,17 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            System.out.println(new Solution().missingNumber(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
 class Solution {
-    int missingNumber(int arr[]) {
-        Arrays.sort(arr);
-        int n = arr.length;
-        // Handle edge case where the missing number is at the beginning
-        if (arr[0] != 1) {
-            return 1;
+    int missingNum(int arr[]) {
+        int n=arr.length;
+        int sum1=0;
+        int sum2=0;
+        
+        for(int i=1;i<=n+1;i++){
+            sum2+=i;
         }
-        // Find the missing number
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i + 1] != arr[i] + 1) {
-                return arr[i] + 1;
-            }
+        for(int j:arr){
+            sum1+=j;
         }
-        // Handle edge case where the missing number is at the end
-        return arr[n - 1] + 1;
+        return (sum2-sum1);
+        
+        
     }
 }
