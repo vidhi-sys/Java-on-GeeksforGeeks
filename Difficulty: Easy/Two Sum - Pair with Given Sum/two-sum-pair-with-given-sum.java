a2884f8964345981121057c153a4dd1e39a27a9f
vidhi udasi <udasividhi18@gmail.com>
2025-07-27 20:42:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.java
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.java
@@ -1,53 +1,15 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-        while (tc-- > 0) {
-            String[] inputLine;
-            inputLine = br.readLine().trim().split(" ");
-            // int n = Integer.parseInt(inputLine[0]);
-            int x = Integer.parseInt(inputLine[0]);
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            int[] arr = new int[tokens.length];
-            for (int i = 0; i < tokens.length; i++) {
-                arr[i] = Integer.parseInt(tokens[i]);
-            }
-
-            boolean ans = new Solution().twoSum(arr, x);
-            System.out.println(ans ? "true" : "false");
-            // System.out.println("~");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     boolean twoSum(int arr[], int target) {
-        if(arr.length<2||arr==null){
-            return false;
+        // code here
+        HashSet<Integer> set= new HashSet<>();
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            int diff=target-arr[i];
+            if(set.contains(diff)){
+                return true;
+            }
+            set.add(arr[i]);
         }
-        boolean ans=false;
-    Set<Integer> seen = new HashSet<>();
-    for( int num:arr){
-        if(seen.contains(target-num)){
-            return true;
-        }
-        seen.add(num);
-    }
-      return false;
+        return false;
     }
 }
