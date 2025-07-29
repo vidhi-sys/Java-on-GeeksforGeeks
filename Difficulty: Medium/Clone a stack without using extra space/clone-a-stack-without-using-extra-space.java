155ee3ed9364dd28dbd10bba6a92fa5485601812
vidhi udasi <udasividhi18@gmail.com>
2025-07-29 11:06:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Clone a stack without using extra space/clone-a-stack-without-using-extra-space.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+
+class Solution {
+    void clonestack(Stack<Integer> st, Stack<Integer> cloned) {
+      for(int num:st){
+          cloned.push(num);
+      }
+      //cloned.reverse();
+    }
+}
