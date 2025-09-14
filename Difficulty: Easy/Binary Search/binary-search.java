4308316909a6a1ea4dce627403e4d546821da613
vidhi udasi <udasividhi18@gmail.com>
2025-09-14 10:35:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary Search/binary-search.java
+++ Difficulty: Easy/Binary Search/binary-search.java
@@ -1,47 +1,22 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        sc.nextLine(); // consume the newline
-        while (t-- > 0) {
-            int k = sc.nextInt();
-            sc.nextLine(); // consume the newline
-            String input = sc.nextLine();
-            String[] strNumbers = input.split(" ");
-            int[] arr = new int[strNumbers.length];
-            for (int i = 0; i < strNumbers.length; i++) {
-                arr[i] = Integer.parseInt(strNumbers[i]);
-            }
-            Solution ob = new Solution();
-            int res = ob.binarysearch(arr, k);
-            System.out.println(res);
-
-            System.out.println("~");
-        }
-        sc.close();
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     public int binarysearch(int[] arr, int k) {
-       int n=arr.length;
-       for(int i=0;i<n;i++){
-           if(arr[i]==k){
-               return i;
-           }
-       }
-       return -1;
-       
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (arr[mid] == k) {
+                result = mid;    // Found a occurrence
+                right = mid - 1; // Continue searching left for first occurrence
+            } else if (arr[mid] < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return result;
     }
 }
