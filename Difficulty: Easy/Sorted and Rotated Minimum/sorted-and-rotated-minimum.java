1bc72cdf720b9187de80ef159b4178cfeab397ab
vidhi udasi <udasividhi18@gmail.com>
2025-09-19 22:49:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    public int findMin(int[] arr) {
+        // complete the function here
+         Arrays.sort(arr);
+        return arr[0];
+    }
+}
