daccffb8ebc3cdde2665ce945a27c594a696886a
vidhi udasi <udasividhi2@gmail.com>
2025-12-04 14:40:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Compare two linked lists/compare-two-linked-lists.java
+++ Difficulty: Easy/Compare two linked lists/compare-two-linked-lists.java
@@ -2,27 +2,40 @@ class Solution {
     int compare(Node head1, Node head2) {
         Node temp1 = head1;
         Node temp2 = head2;
+        String s1="";
+        String s2="";
         
-        while (temp1 != null && temp2 != null) {
-            // Compare current characters
-            if (temp1.data > temp2.data) {
-                return 1;
-            } else if (temp1.data < temp2.data) {
-                return -1;
-            }
+        while (temp1 != null) {
             
-            // Move to next nodes if characters are equal
+            s1+=Character.toString(temp1.data);
+           
             temp1 = temp1.next;
+            
+        }
+        while (temp2 != null) {
+            
+            
+            s2+=Character.toString(temp2.data);
+            
             temp2 = temp2.next;
         }
         
-        // After loop, check if one list is longer
-        if (temp1 != null) {
-            return 1; // list1 is longer
-        } else if (temp2 != null) {
-            return -1; // list2 is longer
-        }
         
-        return 0; // Both lists are identical
+        if(s1.compareTo(s2)>0){
+            return 1;
+        }
+        else if(s1.compareTo(s2)<0){
+            return -1;
+        }
+        else if(s1.length()>s2.length()){
+            return 1;
+        }
+        else  if(s2.length()>s1.length()){
+            return -1;
+        }
+        else if(s1.length()==s2.length()){
+            return 0;
+        }
+        return 1;
     }
 }
