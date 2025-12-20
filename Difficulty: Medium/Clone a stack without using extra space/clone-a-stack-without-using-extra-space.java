db5dbdb31e987878747d8130cd4e92cceabae3c4
vidhi udasi <udasividhi2@gmail.com>
2025-12-20 17:36:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Clone a stack without using extra space/clone-a-stack-without-using-extra-space.java
+++ Difficulty: Medium/Clone a stack without using extra space/clone-a-stack-without-using-extra-space.java
@@ -1,10 +1,18 @@
-// User function Template for Java
-
+import java.util.*;
 class Solution {
     void clonestack(Stack<Integer> st, Stack<Integer> cloned) {
-      for(int num:st){
-          cloned.push(num);
-      }
-      //cloned.reverse();
+        List<Integer> list = new ArrayList<>();
+        
+        // Store all elements in list
+        while (!st.isEmpty()) {
+            list.add(st.pop());
+        }
+        
+        // Push back in reverse order to restore both stacks
+        for (int i = list.size() - 1; i >= 0; i--) {
+            int element = list.get(i);
+            st.push(element);
+            cloned.push(element);
+        }
     }
 }
