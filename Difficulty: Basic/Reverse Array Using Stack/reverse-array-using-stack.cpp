d92c2ab40813e81eecc8387e9ba96c722a8e89b7
vidhi udasi <udasividhi2@gmail.com>
2025-12-21 11:59:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse Array Using Stack/reverse-array-using-stack.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void reverseArray(vector<int>& arr) {
+        reverse(arr.begin(), arr.end());
+    }
+};
